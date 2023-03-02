90c7bc7bd1ef6a019a44a6d81348052e920fa262
Tosei <359387050@qq.com>
2023-03-02 23:27:44 +0900

    feat: add timeout for read and error handler

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 use crossterm::{
-    event::{read, Event::Key, KeyCode},
+    event::{poll, read, Event::Key, KeyCode},
     terminal, Result,
 };
+use errno::errno;
+use std::time::Duration;
 
 fn main() -> Result<()> {
     terminal::enable_raw_mode()?;
-    // loop {
-    while let Ok(event) = read() {
-        if let Key(key_event) = event {
-            if key_event.code == KeyCode::Char('q') {
+
+    loop {
+        let mut c = None;
+        match poll(Duration::from_millis(100)) {
+            Ok(true) => {
+                if let Ok(event) = read() {
+                    if let Key(key_event) = event {
+                        c = Some(key_event);
+                    }
+                } else {
+                    die("read failed");
+                    break;
+                }
+            }
+            Ok(false) => {}
+            _ => {
+                die("poll failed");
                 break;
-            } else {
-                println!("{event:?}\r");
             }
         }
+
+        if let Some(c) = c {
+            if c.code == KeyCode::Char('q') {
+                break;
+            } else {
+                println!("{c:?}\r");
+            }
+        } else {
+            println!("no key\r");
+        }
     }
-    // else {
-    //     break;
-    // }
-    // }
+
     terminal::disable_raw_mode()?;
     Ok(())
+}
+
+fn die<S: Into<String>>(message: S) {
+    let _ = terminal::disable_raw_mode();
+    eprintln!("{} : {}", message.into(), errno());
+    std::process::exit(1);
 }
